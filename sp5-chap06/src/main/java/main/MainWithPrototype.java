d7b48650411d23bbeea6196142a654cbcc3d6cf5
Yohan <yohanpark@medibloc.org>
2019-06-06 16:09:44 +0900

    [chap06] print lines in output

--- sp5-chap06/src/main/java/main/MainWithPrototype.java
+++ sp5-chap06/src/main/java/main/MainWithPrototype.java
@@ -11,12 +11,16 @@ import spring.Client;
 public class MainWithPrototype {
 
 	public static void main(String[] args) throws IOException {
-		AbstractApplicationContext ctx = 
+		AbstractApplicationContext ctx =
 				new AnnotationConfigApplicationContext(AppCtxWithPrototype.class);
+
+		System.out.println("-----");
 
 		Client client1 = ctx.getBean(Client.class);
 		Client client2 = ctx.getBean(Client.class);
 		System.out.println("client1 == client2 : " + (client1 == client2));
+
+		System.out.println("-----");
 
 		ctx.close();
 	}
